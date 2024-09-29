c33692579b7144d6368d61177edc9806f33be14f
giorgi002 <167451976+giorgi002@users.noreply.github.com>
2024-09-29 14:29:36 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+
+// firth task 👇
+
+// function mergeObjects(...objects){
+//     return {...objects[0],...objects[1],...objects[2]}
+// }
+// const obj1 = {a:1, b:2};
+// const obj2 = {b:3, c:5};
+// const obj3 = {c:6, d:7};
+// console.log(mergeObjects(obj1,obj2,obj3));
+
+
+
+// second task 👇
+// function deleteKey(obj,key){
+//     if(key in obj){
+//         delete obj[key]
+//     }
+// }
+// const myObject = {
+//     name:"giorgi",
+//     surname:"nadiradze",
+//     age:17
+// }
+// console.log("before delete", myObject);
+// deleteKey(myObject, "age")
+// console.log("after delete", myObject);
+
+
+// third task 👇
+// const car = {
+//     make:"BMW",
+//     model:"F30",
+//     year:2016,
+//     getdiscription(){
+//         return ` ${this.year} ${this.make} ${this.model}`
+//     }
+// }
+// console.log(car.getdiscription());
+
+
